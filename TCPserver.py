1d458f4b7042d43c1ca9e9fbe2760edcc36d0e45
FettiWop <88058704+FettiWop@users.noreply.github.com>
2023-05-23 15:16:37 +0200

    Update TCPserver.py

--- TCPserver.py
+++ TCPserver.py
@@ -3,22 +3,22 @@
 import socket
 
 serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
+#host = "your local ip'
 host = socket.gethostbyname()
 #the port to connect to.
 port = 444
 #binding address port to socket.
-serversocket.bind((host, port))
+serversocket.bind(('172.20.20.127', port))
 
 #tcp listener connections, here we can change the number of connections we can make to this server.
 serversocket.listen(3)
 while True:
     clientsocket, address = serversocket.accept()
 
-    print("received connection from " % str(address))
+    print("received connection from %s " % str(address))
 
     message = 'Thank you for connecting to the fetti server' + "\r\n"
-    clientsocket.send(message)
+    clientsocket.send(message('ascii'))
 
     clientsocket.close()
 
